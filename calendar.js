2b8a3def7a7fa07ff52bb3268519ff7135a467e4
aplog30 <apollog2004@gmail.com>
2025-11-24 01:26:30 -0500

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -2,7 +2,7 @@
 /************************************************************
  * CONFIGURATION
  ************************************************************/
-const GOOGLE_SHEET_API_URL = ""https://script.google.com/macros/s/AKfycby4FMMdrwqh8NbYxgAMxM-09qUTLfB04oT8SLyu9ffcNaHdQihlPNk8vzsI0dhRcJy5Kg/exec"; // paste your Apps Script URL
+const GOOGLE_SHEET_API_URL = "https://script.google.com/macros/s/AKfycby4FMMdrwqh8NbYxgAMxM-09qUTLfB04oT8SLyu9ffcNaHdQihlPNk8vzsI0dhRcJy5Kg/exec"; // paste your Apps Script URL
 
 // Today and 1-year max limit
 const TODAY = new Date();
